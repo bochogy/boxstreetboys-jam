cc3932408131707787fecc7a876e08cb87bac39d
Joey Afortunado <joeyafortunado@gmail.com>
2021-07-03 11:45:53 +0800

    bsb incubation

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,22 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
+    <h1>Box Break # 1</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/logo.png"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="Boxstreet Boys Logo"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      Participants List
+    </p>
+    <p>
+      Placeholder content here
     </p>
   </Layout>
 )
