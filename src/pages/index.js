5e17b2fb0c3906c8280c5e9c6fca0351bc0b4754
Joey Afortunado <joeyafortunado@gmail.com>
2021-07-03 12:01:33 +0800

    bird

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,11 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Box Break # 1</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Box Break # 1</h1>    
     <StaticImage
       src="../images/logo.png"
       width={300}
@@ -19,13 +18,47 @@ const IndexPage = () => (
       alt="Boxstreet Boys Logo"
       style={{ marginBottom: `1.45rem` }}
     />
+    <p>Register here</p>    
+    <RegisterForm></RegisterForm>
     <p>
       Participants List
     </p>
     <p>
       Placeholder content here
     </p>
+    
   </Layout>
 )
 
 export default IndexPage
+
+class RegisterForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(this.state.value + ': has a big bird.');
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
